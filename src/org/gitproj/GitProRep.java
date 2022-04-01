e637099e86a6259e07cb1ba69dd7790a3794f976
abhinav164 <supriya.lekkalla@gmail.com>
2022-04-01 12:52:56 +0530

    update latest one

---
+++ src/org/gitproj/GitProRep.java
@@ -0,0 +1,10 @@
+
+package org.gitproj;
+
+public class GitProRep {
+	public static void main(String[] args) {
+		System.out.println("move code to remote machine");
+		System.out.println("moved");
+	}
+
+}
